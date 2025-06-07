a3b1ca7f196507407241249685de1685001872f0
Master <gently-oboe-stable@duck.com>
2025-06-07 22:17:42 +0200

    update flakes

--- flake.nix
+++ flake.nix
@@ -3,17 +3,27 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/23.11";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
+          overlays = [ (import rust-overlay) ];
           config = {
             android_sdk.accept_license = true;
             allowUnfree = true;
           };
         };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src" # for rust-analyzer
+            "rust-analyzer"
+            "rustfmt"
+          ];
+        };
+
         buildToolsVersion = "34.0.0";
         androidComposition = pkgs.androidenv.composeAndroidPackages {
           buildToolsVersions = [ buildToolsVersion "30.0.3" ];
@@ -26,11 +36,18 @@
           mkShell rec {
             ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
             buildInputs = [
+              # Mobile
               flutter
               androidSdk # The customized SDK that we've made above
               jdk17
-              bun
+
+              # Backend
+              rust
+
+              # Chore
               just
+              parallel
+              openapi-generator-cli
             ];
           };
       });
